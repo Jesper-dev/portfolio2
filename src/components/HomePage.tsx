cf6667712f5879c5108c5d6d8f5edf47085a4448
JesperKYH <jesper.pettersson@student.kyh.se>
2021-06-29 12:10:03 +0200

    Added icons to the home page with links in them

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,14 +1,38 @@
 // import { useState } from "react";
 
 export const HomePage: React.FC = () => {
+  let linksList: { href: string; icon: string }[] = [
+    { href: "https://github.com/Jesper-dev", icon: "fab fa-github" },
+    {
+      href: "https://www.linkedin.com/in/jesper-pettersson-218245179/",
+      icon: "fab fa-linkedin-in",
+    },
+    { href: "mailto:jesper@jesperp.se", icon: "fas fa-envelope" },
+  ];
+
   return (
     <section className="homePageContainer">
       <div className="textContainer">
         <h1>Hello!</h1>
         <p>
-          My name is Jesper Pettersson and i'm currently studying to become a
-          front-end Developer!
+          My name is Jesper <span>Pettersson</span> and i'm currently studying
+          to become a <span>Front-End Developer</span>!
         </p>
+        <div className="linkContainer">
+          {/* Renders all the icons/links on the home page */}
+          {linksList.map((item, i) => {
+            return (
+              <a
+                key={i}
+                href={item.href}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <i className={item.icon}></i>
+              </a>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
