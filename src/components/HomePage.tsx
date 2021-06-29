e08d6533afbb29137bf1bdd27209ffbf72d2ad85
JesperKYH <jesper.pettersson@student.kyh.se>
2021-06-29 11:36:46 +0200

    Version 1 Of HomePage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,15 @@
-export const HomePage = () => {
-    return (
-        <section className="homePageContainer">
-            <h1>HomePage</h1>
-        </section>
-    )
-}
+// import { useState } from "react";
+
+export const HomePage: React.FC = () => {
+  return (
+    <section className="homePageContainer">
+      <div className="textContainer">
+        <h1>Hello!</h1>
+        <p>
+          My name is Jesper Pettersson and i'm currently studying to become a
+          front-end Developer!
+        </p>
+      </div>
+    </section>
+  );
+};
