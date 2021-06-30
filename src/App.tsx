a48da3f055080319d903306882cb9bdef3f32695
JesperKYH <jesper.pettersson@student.kyh.se>
2021-06-30 10:19:57 +0200

    Added skills page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import './styles/app.scss'
-import { HomePage } from './components/HomePage';
+import React from "react";
+import "./styles/app.scss";
+import { HomePage } from "./components/HomePage";
+import { Skills } from "./components/Skills";
 
 function App() {
   return (
     <main className="pageContainer">
       {/* Componentes goes here */}
       <HomePage />
+      <Skills />
     </main>
   );
 }
---
+++ src/components/Skills.tsx
@@ -0,0 +1,29 @@
+export const Skills: React.FC = () => {
+  let iconsList: { icon: string; style: string; text: string }[] = [
+    { icon: "fab fa-html5", style: "red", text: "HTML" },
+    { icon: "fab fa-css3", style: "blue", text: "CSS3" },
+    { icon: "fab fa-sass", style: "pink", text: "SASS" },
+    { icon: "fab fa-js-square", style: "yellow", text: "JAVASCRIPT" },
+    { icon: "fab fa-react", style: "blue", text: "REACT" },
+    { icon: "fab fa-git-alt", style: "crimson", text: "GIT" },
+    { icon: "fab fa-github-square", style: "black", text: "GITHUB" },
+  ];
+
+  return (
+    <section className="skillsContainer">
+      <div className="titleContainer">
+        <h1>Skills</h1>
+      </div>
+      <div className="skillsIconsWrapper">
+        {iconsList.map((item, i) => {
+          return (
+            <div key={i} className="iconWrapper">
+              <i className={item.icon} style={{ color: item.style }}></i>
+              <p>{item.text}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
