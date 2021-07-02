9239e39c487e77f65131c20227a3aeeb305ea3b1
JesperKYH <jesper.pettersson@student.kyh.se>
2021-07-02 10:09:19 +0200

    Added dark theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,24 @@
 import { Link } from "react-scroll";
+import { useState, useEffect } from "react";
 
 export const Navbar: React.FC = () => {
+  const [themeDark, setThemeDark] = useState(false);
+
+  useEffect(() => {
+    const getTheme = localStorage.getItem("theme");
+    if (getTheme === "dark") setThemeDark(true);
+  }, []);
+
+  useEffect(() => {
+    if (themeDark) {
+      localStorage.setItem("theme", "dark");
+      document.body.classList.add("darkTheme");
+    } else {
+      localStorage.setItem("theme", "light");
+      document.body.classList.remove("darkTheme");
+    }
+  }, [themeDark]);
+
   let links: { text: string }[] = [
     { text: "home" },
     { text: "about" },
@@ -18,7 +36,12 @@ export const Navbar: React.FC = () => {
       })}
       <div className="themeSwitchWrapper">
         <label className="themeSwitch" htmlFor="checkbox">
-          <input type="checkbox" id="checkbox" />
+          <input
+            type="checkbox"
+            id="checkbox"
+            onChange={() => setThemeDark(!themeDark)}
+            checked={themeDark}
+          />
           <div className="slider round"></div>
         </label>
       </div>
