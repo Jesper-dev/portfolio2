a35589b2b593ceba9bf93911af656987225d321b
JesperKYH <jesper.pettersson@student.kyh.se>
2022-12-19 20:47:41 +0100

    Updated aboutMe text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,10 +7,9 @@ export const About: React.FC = () => {
       <div className="contentContainer" data-aos="flip-down">
         <p>
           My name is <em>Jesper Pettersson</em> and I'm from{" "}
-          <strong>Sweden, Uppsala</strong>. I'm currently studying to become a
-          front-end developer at <strong>KYH, Stockholm</strong>. My journey to
-          become a web developer started around one year ago. My interests
-          include space, our earth, such as nature and animals, and of course
+          <strong>Sweden, Uppsala</strong>. I'm currently working at{" "}
+          <strong>Caspeco AB, Uppsala</strong> and have been working there for 1
+          year. My interests include movies, reading, cooking and of course
           programming. I love to do all kinds of different projects but the ones
           that challenge me and makes me become a better webdeveloper are my
           favourites. I enjoy working in teams but working alone is also an
